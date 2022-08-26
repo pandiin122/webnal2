2bb27d22e361d9075bc2ccd2c3af4b412f6ba733
pandiin122 <37384391+pandiin122@users.noreply.github.com>
2022-08-26 10:59:22 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db="blog_samples1";
+$servername = "	us-cdbr-east-06.cleardb.net";
+$username = "b79c0e5ef2f44c";
+$password = "988326d0";
+$db="heroku_79e761ab428f3d3";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$db);
