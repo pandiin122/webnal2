9ce31e7fe8b2a4bad1595ad8a7032bdd68c83a80
pandiin122 <37384391+pandiin122@users.noreply.github.com>
2022-08-26 11:01:23 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "	us-cdbr-east-06.cleardb.net";
+$servername = "us-cdbr-east-06.cleardb.net";
 $username = "b79c0e5ef2f44c";
 $password = "988326d0";
 $db="heroku_79e761ab428f3d3";
